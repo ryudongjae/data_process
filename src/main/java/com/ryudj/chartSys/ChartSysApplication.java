8eb30c9ba4b13f29d46f9fc6efe7057da0504290
ryudongjae <rdj10149@gmail.com>
2022-02-18 12:31:28 +0900

    Initial commit

---
+++ src/main/java/com/ryudj/chartSys/ChartSysApplication.java
@@ -0,0 +1,13 @@
+package com.ryudj.chartSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartSysApplication.class, args);
+	}
+
+}
